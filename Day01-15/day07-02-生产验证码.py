709de559dae27de4c37095d4697311f43908b6d6
HUAWEI <330593471@qq.com>
2024-01-19 14:46:34 +0800

    random study

---
+++ Day01-15/day07-02-生产验证码.py
@@ -0,0 +1,26 @@
+import random
+
+
+def main():
+    print(generate_code())
+    print(generate_code(7))
+
+
+
+def generate_code(code_len=4):
+    """
+    生成指定长度的验证码
+    :param code_len: 验证码的长度（默认四个字符）
+    :return: 由大小写英文字母和数字构成的随机验证码
+    """
+    all_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    last_pos = len(all_chars) - 1
+    code = ''
+    for _ in range(code_len):
+        index = random.randint(0, last_pos)
+        code += all_chars[index]
+    return code
+
+
+if __name__ == '__main__':
+    main()
